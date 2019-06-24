d7d1da3992eb074f2b96c8d33fd2d8992a37b59f
MrMachine <47859623+gstvlopesXV@users.noreply.github.com>
2019-06-24 12:51:31 -0300

    Add files via upload

---
+++ MySQL_JAVA/DB_Connect.java
@@ -0,0 +1,41 @@
+package pkg;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*import java.util.logging.Level;
+import java.util.logging.Logger;
+*/
+
+import javax.swing.JOptionPane;
+	
+public class DB_Connect {
+	
+	public static void main(String[] args) {
+		DB_Connect pro = new DB_Connect();
+		pro.createConnection();
+		
+	}
+	
+	void createConnection(){
+		final String DRIVER = "com.mysql.jdbc.Driver";
+		final String URL = "jdbc:mysql://localhost:3306/db_supermercado";
+		final String USER = "root";
+		final String PASSWORD = "";
+		
+		try {
+			Class.forName(DRIVER);
+			Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+			
+		} catch	(ClassNotFoundException e) {
+			//Logger.getLogger(db_connect.class.getName()).log(Level.SEVERE,null, e);
+			JOptionPane.showMessageDialog(null, "Driver não encontrado!\n" + e);
+			
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Problemas na conexão com o banco\n" + e);
+		}
+	
+	}
+	
+}
